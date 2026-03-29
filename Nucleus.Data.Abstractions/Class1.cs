4729f7d2e65acfcc3764bd03096581eed48ac6df
AbdulrahmanFrontend <abdulrahman.abdelstar@gmail.com>
2026-03-29 20:16:52 +0200

    Add project files.

---
+++ Nucleus.Data.Abstractions/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nucleus.Data.Abstractions
+{
+    public class Class1
+    {
+    }
+}
---
+++ Nucleus.Data.Tests/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nucleus.Data.Tests
+{
+    public class Class1
+    {
+    }
+}
